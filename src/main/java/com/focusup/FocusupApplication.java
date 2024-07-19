2e811a5d2e80dabd71e2957a267a45fc43d0470b
heejjinkim <06.hjhj.12@gmail.com>
2024-07-19 16:44:06 +0900

    init project

---
+++ src/main/java/com/focusup/FocusupApplication.java
@@ -0,0 +1,13 @@
+package com.focusup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusupApplication.class, args);
+	}
+
+}
