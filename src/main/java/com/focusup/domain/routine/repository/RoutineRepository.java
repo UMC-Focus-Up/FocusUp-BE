7a5ab02ec6d4a2cee058a58c32d4d945c01928f7
EunJiJung <bianbbc87@gmail.com>
2024-07-24 11:26:42 +0900

    [FEAT] routine, userroutine repository 정의

--- src/main/java/com/focusup/domain/routine/repository/RoutineRepository.java
+++ src/main/java/com/focusup/domain/routine/repository/RoutineRepository.java
@@ -1,9 +1,16 @@
 package com.focusup.domain.routine.repository;
 
 import com.focusup.entity.Routine;
+import com.focusup.entity.User;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
+@NonNullApi
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
+    Optional<Routine> findById(Long id);
 }
--- src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
+++ src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
@@ -1,9 +1,17 @@
 package com.focusup.domain.routine.repository;
 
+import com.focusup.entity.Routine;
+import com.focusup.entity.User;
 import com.focusup.entity.UserRoutine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface UserRoutineRepository extends JpaRepository<UserRoutine, Long> {
+    List<UserRoutine> findByUser(User user);
+
+    List<UserRoutine> findByDate(LocalDate date);
 }
