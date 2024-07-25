9834e19b5923efbc403a59dc3dc1b757f18537ca
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-07-25 14:26:19 +0900

    [REFACTOR #6] 레벨 변경 api 설명 추가

--- src/main/java/com/focusup/domain/level/controller/LevelController.java
+++ src/main/java/com/focusup/domain/level/controller/LevelController.java
@@ -6,17 +6,19 @@ import com.focusup.domain.level.dto.LevelResponse;
 import com.focusup.domain.level.service.LevelService;
 import com.focusup.entity.LevelHistory;
 import com.focusup.global.apiPayload.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/level")
+@RequestMapping("/level")
 public class LevelController {
     private final LevelService levelService;
 
     @PutMapping("/{userId}")
+    @Operation(summary = "레벨 변경 API")
     public Response<LevelResponse.NewLevelResultDTO> putNewLevel (@PathVariable("userId") Long userId, @RequestParam(name = "level") Long level) {
         if (level == 0) {
             LevelHistory levelHistory = levelService.findLevel(userId);
