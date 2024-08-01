86ed01e58a8fcd22a0109e52b7a1125fd88f9d0b
heejjinkim <06.hjhj.12@gmail.com>
2024-08-01 20:47:16 +0900

    [FEAT #1] Auth, Token Exception 추가

--- src/main/java/com/focusup/global/apiPayload/exception/TokenException.java
+++ src/main/java/com/focusup/global/apiPayload/exception/TokenException.java
@@ -2,12 +2,12 @@ package com.focusup.global.apiPayload.exception;
 
 import com.focusup.global.apiPayload.code.ErrorCode;
 
-public class MemberException extends CustomException {
-    public MemberException(ErrorCode errorCode, String message) {
+public class TokenException extends CustomException {
+    public TokenException(ErrorCode errorCode, String message) {
         super(errorCode, message);
     }
 
-    public MemberException(ErrorCode errorCode) {
+    public TokenException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
