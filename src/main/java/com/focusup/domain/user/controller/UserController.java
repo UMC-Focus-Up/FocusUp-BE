03926a25f6af7fe485a730c3eb990377839bb33e
Seoyeon Hong <hsy2709@gmail.com>
2024-08-02 22:02:25 +0900

    [FEAT #16] 홈, 캐릭터 화면 조회 기능

--- src/main/java/com/focusup/domain/user/controller/UserController.java
+++ src/main/java/com/focusup/domain/user/controller/UserController.java
@@ -1,11 +1,30 @@
 package com.focusup.domain.user.controller;
 
+import com.focusup.domain.user.service.UserService;
+import com.focusup.global.apiPayload.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/home")
+    @Operation(summary = "홈 조회 api")
+    public Response<?> getHomeInfo(@RequestParam Long userId){
+        return Response.success(userService.getHomeInfo(userId));
+    }
+
+    @GetMapping("/character")
+    @Operation(summary = "캐릭터 화면 조회")
+    public Response<?> getCharacterPageInfo(@RequestParam Long userId){
+        return Response.success(userService.getCharacterPageInfo(userId));
+    }
 }
