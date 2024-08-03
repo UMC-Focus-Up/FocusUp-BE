24d6722e3bc314afae1ce262cf7d4b0b2603c056
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-08-03 22:07:12 +0900

    [FIX #9] level 관련 user 에러 해결

--- src/main/java/com/focusup/domain/level/service/LevelService.java
+++ src/main/java/com/focusup/domain/level/service/LevelService.java
@@ -5,22 +5,18 @@ import com.focusup.domain.level.repository.LevelRepository;
 import com.focusup.domain.user.repository.UserRepository;
 import com.focusup.entity.Level;
 import com.focusup.entity.LevelHistory;
+import com.focusup.entity.User;
 import com.focusup.global.apiPayload.code.ErrorCode;
 import com.focusup.global.apiPayload.exception.LevelException;
 import com.focusup.global.apiPayload.exception.MemberException;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class LevelService {
-    @PersistenceContext
-    private final EntityManager em;
 
     private final UserRepository userRepository;
     private final LevelRepository levelRepository;
@@ -29,7 +25,7 @@ public class LevelService {
     @Transactional
     public LevelHistory findLevel (String oauthId) {
         User user = userRepository.findByOauthId(oauthId).orElseThrow(() -> new MemberException(ErrorCode.USER_NOT_FOUND));
-        LevelHistory levelHistory = levelHistoryRepository.findByUserId(user.getId());
+        LevelHistory levelHistory = levelHistoryRepository.findByUser(user);
 
         return levelHistory;
     }
