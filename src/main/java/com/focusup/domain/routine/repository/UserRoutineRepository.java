d6168e0427a1f214b890d96d8ea0fca84aeb5638
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-08-14 20:07:41 +0900

    [FIX] 전체 상위 루틴 리스트 조회 유저 추가

--- src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
+++ src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Repository
 public interface UserRoutineRepository extends JpaRepository<UserRoutine, Long> {
     List<UserRoutine> findByUser(User user);
+    List<UserRoutine> findByUserOrderByStartDateAsc(User user);
     List<UserRoutine> findByRoutines(Routine routine);
     Optional<UserRoutine> findById(Long id);
 
