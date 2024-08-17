d98ede63ad090982dfb0bfc1489c6b7fc1e6f6cd
EunJiJung <bianbbc87@gmail.com>
2024-08-17 14:52:54 +0900

    fix : repeat cycle 미출력 문제 fix
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
+++ src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
@@ -18,6 +18,8 @@ public interface UserRoutineRepository extends JpaRepository<UserRoutine, Long> 
     List<UserRoutine> findByUser(User user);
     List<UserRoutine> findByUserOrderByStartDateAsc(User user);
     Optional<UserRoutine> findById(Long id);
+    @Query("SELECT ur FROM UserRoutine ur JOIN FETCH ur.repeatCycleDay WHERE ur.id = :id")
+    Optional<UserRoutine> findByIdWithCycleDays(@Param("id") Long id);
 
     Page<UserRoutine> findAllByUser(User user, Pageable pageable);
 
