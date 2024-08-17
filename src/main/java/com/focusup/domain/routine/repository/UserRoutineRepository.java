cba0cd3887ea16331b45e4ed46f1ea9ee150a628
EunJiJung <bianbbc87@gmail.com>
2024-08-17 15:16:09 +0900

    repository find method 변경
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
+++ src/main/java/com/focusup/domain/routine/repository/UserRoutineRepository.java
@@ -18,8 +18,6 @@ public interface UserRoutineRepository extends JpaRepository<UserRoutine, Long> 
     List<UserRoutine> findByUser(User user);
     List<UserRoutine> findByUserOrderByStartDateAsc(User user);
     Optional<UserRoutine> findById(Long id);
-    @Query("SELECT ur FROM UserRoutine ur LEFT JOIN FETCH ur.repeatCycleDay WHERE ur.id = :id")
-    Optional<UserRoutine> findByIdWithCycleDays(@Param("id") Long id);
 
     Page<UserRoutine> findAllByUser(User user, Pageable pageable);
 
