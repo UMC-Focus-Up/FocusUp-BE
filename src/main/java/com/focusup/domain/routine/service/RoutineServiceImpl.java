b6db624ca2adc9ecf662b4db1601f10c79b5656c
EunJiJung <bianbbc87@gmail.com>
2024-08-18 18:49:31 +0900

    fix : boostCount 계산 로직 삭제
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/main/java/com/focusup/domain/routine/service/RoutineServiceImpl.java
+++ src/main/java/com/focusup/domain/routine/service/RoutineServiceImpl.java
@@ -118,7 +118,7 @@ public class RoutineServiceImpl implements RoutineService{
         // 유저의 level successcount 확인
         LevelHistory levelHistory = levelHistoryRepository.findByUser(user);
 
-        int boostCount = (int) execTime / 10;
+        int boostCount = (int) execTime;
         levelHistory.addSuccessCount(boostCount);
 
 // boostCount가 5 이상이면 레벨 업
