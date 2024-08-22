f0d6740b1fe81b50c007ff159d7dc92738d1a42b
EunJiJung <bianbbc87@gmail.com>
2024-08-22 12:08:53 +0900

    fix : totalTime에서 음수 출력 문제 fix
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/main/java/com/focusup/domain/routine/service/RoutineServiceImpl.java
+++ src/main/java/com/focusup/domain/routine/service/RoutineServiceImpl.java
@@ -117,7 +117,7 @@ public class RoutineServiceImpl implements RoutineService{
         Routine routine = routineRepository.findById(routineId).orElseThrow(() -> new RoutineException(ErrorCode.ROUTINE_NOT_FOUND));
 
         // 전체 루틴 시간 (분 단위)
-        long totalTime = Duration.between(routine.getUserRoutine().getStartTime(), routine.getUserRoutine().getGoalTime()).toMinutes();
+        long totalTime = Duration.between(routine.getUserRoutine().getGoalTime(), routine.getUserRoutine().getStartTime()).toMinutes();
         // 실행 시간 (분 단위)
         long execTime = Duration.between(LocalTime.MIDNIGHT, request.getExecTime()).toMinutes();
         // 달성률 계산
